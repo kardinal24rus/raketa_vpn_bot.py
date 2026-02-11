ff2b91716221a3fde4a18f61c7592846eddd288c
kardinal24rus <klushkinnmail@gmail.com>
2026-02-12 02:58:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import init_db
 
-# Импортируем роутеры
+# Импорт роутеров
 from handlers.start import router as start_router
 from handlers.profile import router as profile_router
 from handlers.payments import router as payments_router
@@ -12,17 +12,19 @@ async def main():
     # Инициализация базы данных
     init_db()
 
+    # Создаем бот и диспетчер
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    # Подключаем роутеры
+    # Подключаем все роутеры
     dp.include_router(start_router)
     dp.include_router(profile_router)
     dp.include_router(payments_router)
 
     print("Бот запущен...")
 
+    # Запуск polling
     await dp.start_polling(bot)
 
-if name == "__main__":
+if __name__ == "__main__":
     asyncio.run(main())
