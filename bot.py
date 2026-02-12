69da41039f279e20fa1a3f2b4c4e79f3094f985d
kardinal24rus <klushkinnmail@gmail.com>
2026-02-12 08:20:35 +0700

    Update and rename main.py to bot.py

--- bot.py
+++ bot.py
@@ -3,28 +3,20 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import init_db
 
-# Импорт роутеров
 from handlers.start import router as start_router
 from handlers.profile import router as profile_router
 from handlers.payments import router as payments_router
 
 async def main():
-    # Инициализация базы данных
     init_db()
-
-    # Создаем бот и диспетчер
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
-
-    # Подключаем все роутеры
     dp.include_router(start_router)
     dp.include_router(profile_router)
     dp.include_router(payments_router)
 
     print("Бот запущен...")
-
-    # Запуск polling
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
+if name == "__main__":
     asyncio.run(main())
