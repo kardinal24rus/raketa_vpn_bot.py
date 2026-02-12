5f3bd5781a29b34dc34a32713ed64850e854e582
kardinal24rus <klushkinnmail@gmail.com>
2026-02-12 15:21:37 +0700

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN не найден в переменных окружения")
-
-DB_PATH = os.getenv("DB_PATH", "bot_database.sqlite3")
